13712fda6044a480897e2053c09bed9e643cb99d
code-blogger3 <itric46@gmail.com>
2024-04-14 17:47:19 +0545

    input field integrate

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,43 @@ import { Select,  SelectContent,
   SelectTrigger,
   SelectValue, } from './components/ui/select'
 import { initialState, todoCategoryOptions, todoReducer } from './helpers/newTodo';
+import { Input } from './components/ui/input';
+import { Button } from './components/ui/button';
 
 function App() {
   const [state, dispatcher] = useReducer(todoReducer, initialState);
 
-  const handleChange = (value) => {
+  const handleChangeSelect = (value) => {
     dispatcher({
       type: "CHANGE_INPUT",
       payload: { name: 'todoCategory', value }, // Hardcode the name or pass it from the component
+    });
+  };
+  const handleChange = (e) => {
+    dispatcher({
+      type: "CHANGE_INPUT",
+      payload: { name: e.target.name, value: e.target.value },
     });
   };
   console.log(state)
 
   return (
     <>
+     <Input
+              type="text"
+              placeholder="Enter Category"
+              name="todoCategory"
+              id=""
+              value={state.todoCategory}
+              onChange={handleChange}
+              // defaultValue={state.todoCategory}
+            />
+            
       <Select
             name="todoCategory"
             id=""
             value={state.todoCategory}
-           onValueChange={handleChange}
+           onValueChange={handleChangeSelect}
           >
             <SelectTrigger className="w-[280px]">
               <SelectValue placeholder="Select a Category" />
