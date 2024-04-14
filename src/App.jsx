195bad64bdcb68f4542c5cc6af3dceb325572974
code-blogger3 <itric46@gmail.com>
2024-04-14 12:29:16 +0545

    select test first phase passed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
-import { useState } from 'react'
+import { useReducer, useState } from 'react'
 import './App.css'
+import { Select,  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue, } from './components/ui/select'
+import { initialState, todoCategoryOptions, todoReducer } from './helpers/newTodo';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [state, dispatcher] = useReducer(todoReducer, initialState);
+  const [value,setValue] = useState('')
+  const handleChange = (newValue) => {
+    setValue(newValue);
+  };
+  console.log(value)
 
   return (
     <>
-      
+      <Select
+            name="todoCategory"
+            id=""
+            value={value}
+           onValueChange={handleChange}
+          >
+            <SelectTrigger className="w-[280px]">
+              <SelectValue placeholder="Select a Category" />
+            </SelectTrigger>
+            <SelectContent>
+              {todoCategoryOptions.map((option) => (
+                <SelectItem key={option.value} value={option.value}>
+                  {option.label}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
     </>
   )
 }
---
+++ src/helpers/newTodo.js
@@ -0,0 +1,72 @@
+const initialState = {
+    id: "",
+    name: "",
+    completed: false,
+    importantUrgentCategory: "None",
+    todoCategory: "None",
+    localPriorityText: "None",
+    localPriorityNum: 0,
+    globalPriorityText: "None",
+    globalPriorityNum: 0,
+  };
+  
+  const ACTIONS = {
+    CHANGE_INPUT: "CHANGE_INPUT",
+    CHANGE_SELECT: "CHANGE_SELECT",
+  };
+  
+  const todoReducer = (state, action) => {
+    switch (action.type) {
+      case ACTIONS.CHANGE_INPUT:
+        return {
+          ...state,
+          [action.payload.name]: action.payload.value,
+        };
+      case ACTIONS.CHANGE_SELECT: // Add a new case for the select component's action
+        return {
+          ...state,
+          todoCategory: action.payload, // Update the todoCategory value in the state
+        };
+      default:
+        return state;
+    }
+  };
+  
+  const ImpUrgCategoryOptions = [
+    { value: "none", label: "None" },
+    { value: "importantUrgent", label: "importantUrgent" },
+    { value: "notImportantUrgent", label: "Not important and Urgent" },
+    { value: "importantNotUrgent", label: "important and not Urgent" },
+    { value: "notImportanNottUrgent", label: "Not important and Not Urgent" },
+  ];
+  
+  const todoCategoryOptions = [
+    { value: "none", label: "None" },
+    {
+      value: "study",
+      label: "Study",
+    },
+    { value: "code", label: "Code" },
+  ];
+  
+  const PriorityOptions = [
+    {
+      value: "none",
+      label: "None",
+    },
+    { value: "highest", label: "Highest" },
+    {
+      value: "lowest",
+      label: "Lowest",
+    },
+  ];
+  
+  export {
+    todoReducer,
+    todoCategoryOptions,
+    ImpUrgCategoryOptions,
+    PriorityOptions,
+    initialState,
+    ACTIONS,
+  };
+  
