001f16c33e3adc70430b779ab2c41ff01911d711
code-blogger3 <itric46@gmail.com>
2024-06-30 20:10:48 +0545

    final

--- src/demo.jsx
+++ src/demo.jsx
@@ -1,13 +1,29 @@
-const [isModalOpen, setIsModalOpen] = useState(false);
-    return (
-      <div className="someTaliwindCSS">
-        <button
-          onClick={() => {
-            setIsModalOpen(true);
-          }}
-          className="someTaliwindCSS"
-        >
-          Open Modal
-        </button>
+import React from 'react'
+
+export default function demo() {
+
+const [isModalOpen, setIsModalOpen]
+       = useState(false)
+  return (
+    <div>
+        <button onClick={() => {setIsModalOpen(true)}}
+        > Open Modal </button>
+ {  /  CSS is not displayed as it is not our focus /}
+        {isModalOpen &&
+        <div onClick={(e) => {
+          if (e.target !== e.currentTarget) {
+            return;
+          }
+          setIsModalOpen(false);
+        }}>
+          <div>
+            <h3>Modal</h3>
+        <button onClick={(e)=> setIsModalOpen(false)}
+            > &times; </button>
+                 {/* Modal Content here */}
+          </div>
         </div>
-    )
+          }
+     </div>
+  )
+}
